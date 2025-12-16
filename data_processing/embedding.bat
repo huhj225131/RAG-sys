4371938e92588ac6699efa22f10b4a5ff22e0e3b
huhj225131 <hung20225131@gmail.com>
2025-12-16 20:28:50 +0700

    xu ly pdf data

---
+++ data_processing/embedding.bat
@@ -0,0 +1,13 @@
+@echo off
+set PERSIST_DIR=chroma_store
+set COLLECTION=emb
+set DATA_DIR=crawl
+
+python embedding.py ^
+  --persist-dir "%PERSIST_DIR%" ^
+  --collection "%COLLECTION%" ^
+  --data-dir "%DATA_DIR%" ^
+  --mode  "semantic" ^
+  
+
+
